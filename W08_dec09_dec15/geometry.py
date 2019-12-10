0874cef5c7c3932a59f34bae69124bb80948a4d5
ArthurTndr <arthur.tondereau@insa-lyon.fr>
2019-12-10 17:56:50 +0100

    pep8 corrections

--- W08_dec09_dec15/geometry.py
+++ W08_dec09_dec15/geometry.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 
-## Geometric operations
+# Geometric operations
+
+
 def det(a, b):
     return a.real * b.imag - a.imag * b.real
+
 
 def akward_det(p, q, r):
     a = det(p, q)
@@ -10,11 +13,13 @@ def akward_det(p, q, r):
     c = det(q, r)
     return a - b + c
 
+
 def point_left_line(p, q, r):
     '''
     return True iff r lies to the left of the segment [p, q]
     '''
     return akward_det(p, q, r) >= 0
+
 
 def point_strictly_left_line(p, q, r):
     '''
@@ -23,11 +28,13 @@ def point_strictly_left_line(p, q, r):
     '''
     return akward_det(p, q, r) > 0
 
+
 def point_right_line(p, q, r):
     '''
     return True iff r lies to the right of the segment [p, q]
     '''
     return akward_det(p, q, r) <= 0
+
 
 def point_strictly_right_line(p, q, r):
     '''
@@ -36,11 +43,13 @@ def point_strictly_right_line(p, q, r):
     '''
     return akward_det(p, q, r) < 0
 
+
 def point_on_line(p, q, r):
     '''
     return True iff r lies on the line (p, q),
     '''
     return akward_det(p, q, r) == 0
+
 
 def quick_sort(L, order):
     def switch(i, j):
@@ -65,10 +74,12 @@ def quick_sort(L, order):
 
     aux(0, len(L))
 
+
 def point_left_point(a, b):
     if(a.real == b.real):
         return a.imag >= b.imag
     return a.real < b.real
+
 
 def left_right_sort(L):
     '''
@@ -77,8 +88,10 @@ def left_right_sort(L):
     '''
     quick_sort(L, point_left_point)
 
-## Displays
-def plot_points(L, col = 'k'):
+# Displays
+
+
+def plot_points(L, col='k'):
     '''
     plots all the points of L
     colors : 'k' black
@@ -91,7 +104,8 @@ def plot_points(L, col = 'k'):
 
     plt.plot(X, Y, col + '.')
 
-def plot_chain(L, col = 'k'):
+
+def plot_chain(L, col='k'):
     '''
     plots the segemnts [L[i], L[i+1]] for i=0 to len(L)-2
     colors : 'k' black
@@ -102,9 +116,10 @@ def plot_chain(L, col = 'k'):
     plot_points(L, col)
 
     for i in range(len(L) - 1):
-        plt.plot([L[i].real, L[i+1].real], [L[i].imag, L[i+1].imag], col)
+        plt.plot([L[i].real, L[i + 1].real], [L[i].imag, L[i + 1].imag], col)
 
-def plot_polygon(P, col = 'k'):
+
+def plot_polygon(P, col='k'):
     '''
     plots the polygon P
     colors : 'k' black
@@ -115,15 +130,18 @@ def plot_polygon(P, col = 'k'):
     plot_points(L, col)
 
     for i in range(len(L) - 1):
-        plt.plot([L[i].real, L[i+1].real], [L[i].imag, L[i+1].imag], col)
+        plt.plot([L[i].real, L[i + 1].real], [L[i].imag, L[i + 1].imag], col)
     plt.plot([L[0].real, L[-1].real], [L[0].imag, L[-1].imag], col)
+
 
 def show():
     plt.axis('equal')
     plt.show()
 
-## The convex hulls
-## Algorithms from the presentation
+# The convex hulls
+# Algorithms from the presentation
+
+
 def Jarvis(L):
     CH = []
     # compute CH here
@@ -131,6 +149,7 @@ def Jarvis(L):
     plot_points(L)
     plot_polygon(CH, 'r')
     show()
+
 
 def Usual_algo(L):
     CH = []
@@ -144,7 +163,9 @@ def Usual_algo(L):
     plot_polygon(CH, 'r')
     show()
 
-## Now some exercises
+# Now some exercises
+
+
 def Convex_hull_of_a_polygon(P):
     '''
     P is a polygon (!)
@@ -156,6 +177,7 @@ def Convex_hull_of_a_polygon(P):
     plot_polygon(P)
     plot_polygon(CH, 'r')
     show()
+
 
 def Divide_and_conqeer_convex_hull(L):
     '''
@@ -169,10 +191,3 @@ def Divide_and_conqeer_convex_hull(L):
     plot_points(L)
     plot_polygon(CH, 'r')
     show()
-
-
-
-
-
-
-
